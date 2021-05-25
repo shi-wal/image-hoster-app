ba1767f0b8d8aa6a398939af5de4f430e9bdd946
Shivangi Agrawal <shivangiagrawal667@gmail.com>
2021-05-25 14:33:43 +0530

    signup endpoint completed

--- technical-api/target/generated-sources/com/upgrad/technical/api/model/SignupUserResponse.java
+++ technical-api/target/generated-sources/com/upgrad/technical/api/model/SignupUserResponse.java
@@ -13,7 +13,7 @@ import javax.validation.constraints.*;
  * SignupUserResponse
  */
 @Validated
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2021-05-24T17:02:52.543+05:30")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2021-05-25T10:05:53.475+05:30")
 
 public class SignupUserResponse   {
   @JsonProperty("id")
